0659dbaaa7a93dfc0afddadc7dc5a72b8b98484d
Komeil Majidi <k.majidi@esmcell.com>
2023-09-29 02:59:45 +0330

    i2c patch conditions

--- i2c/i2c-patch.lua
+++ i2c/i2c-patch.lua
@@ -1,3 +1,4 @@
+require("hibernation");
 
 module(..., package.seeall);
 
@@ -68,6 +69,91 @@ end;
 
 if json and json.decode then
 	json.decode = safeJsonDecode;
+end;
+
+local oldUartWrite = uart.write;
+uart.write = function(...)
+	pm.wake("lib.patch.uart.write");
+	local result = oldUartWrite(...);
+	pm.sleep("lib.patch.uart.write");
+	return result;
+end;
+
+if i2c and i2c.write then
+	local oldI2cWrite = i2c.write;
+	i2c.write = function(...)
+		pm.wake("lib.patch.i2c.write");
+		local result = oldI2cWrite(...);
+		pm.sleep("lib.patch.i2c.write");
+		return result;
+	end;
+end;
+
+if i2c and i2c.send then
+	local oldI2cSend = i2c.send;
+	i2c.send = function(...)
+		pm.wake("lib.patch.i2c.send");
+		local result = oldI2cSend(...);
+		pm.sleep("lib.patch.i2c.send");
+		return result;
+	end;
+end;
+
+if spi and spi.send then
+	oldSpiSend = spi.send;
+	spi.send = function(...)
+		pm.wake("lib.patch.spi.send");
+		local result = oldSpiSend(...);
+		pm.sleep("lib.patch.spi.send");
+		return result;
+	end;
+end;
+
+if spi and spi.send_recv then
+	oldSpiSendRecv = spi.send_recv;
+	spi.send_recv = function(...)
+		pm.wake("lib.patch.spi.send_recv");
+		local result = oldSpiSendRecv(...);
+		pm.sleep("lib.patch.spi.send_recv");
+		return result;
+	end;
+end;
+
+if disp and disp.sleep then
+	oldDispSleep = disp.sleep;
+	disp.sleep = function(...)
+		pm.wake("lib.patch.disp.sleep");
+		oldDispSleep(...);
+		pm.sleep("lib.patch.disp.sleep");
+	end;
+end;
+
+if io and io.mount then
+	oldIoMount = io.mount;
+	io.mount = function(...)
+		pm.wake("lib.patch.io.mount");
+		local result = oldIoMount(...);
+		pm.sleep("lib.patch.io.mount");
+		return result;
+	end;
+end;
+
+local pmdInited;
+if pmd and pmd.init then
+	oldPmdInit = pmd.init;
+	pmd.init = function(...)
+		if not pmdInited then
+			pmdInited = true;
+		end;
+		local result = oldPmdInit(...);
+		return result;
+	end;
+end;
+
+pmd.libScriptInit = function()
+	if not pmdInited then
+		pmd.init({});
+	end;
 end;
 
 
