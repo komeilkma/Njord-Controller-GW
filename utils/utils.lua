b40a98bafedd2ba7e42a61078d8f1e0c72bccebc
Komeil Majidi <k.majidi@esmcell.com>
2023-09-28 22:49:57 +0330

    add utils on system

--- utils/utils.lua
+++ utils/utils.lua
@@ -97,4 +97,110 @@ function string.formatNumberThousands(num)
 	return formatted;
 end;
 
+function string.split(str, delimiter)
+	local strlist, tmp = {}, string.byte(delimiter);
+	if delimiter == "" then
+		for i = 1, #str do
+			strlist[i] = str:sub(i, i);
+		end;
+	else
+		for substr in string.gmatch(str .. delimiter, "(.-)" .. ((tmp > 96 and tmp < 123 or tmp > 64 and tmp < 91 or tmp > 47 and tmp < 58) and delimiter or "%" .. delimiter)) do
+			table.insert(strlist, substr);
+		end;
+	end;
+	return strlist;
+end;
 
+function string.checkSum(str, num)
+	assert(type(str) == "string", "The first argument is not a string!");
+	local sum = 0;
+	for i = 1, #str do
+		sum = sum + (str:sub(i, i)):byte();
+	end;
+	if num == 2 then
+		return sum % 65536;
+	else
+		return sum % 256;
+	end;
+end;
+
+function io.exists(path)
+	local file = io.open(path, "r");
+	if file then
+		io.close(file);
+		return true;
+	end;
+	return false;
+end;
+
+function io.readFile(path)
+	local file = io.open(path, "rb");
+	if file then
+		local content = file:read("*a");
+		io.close(file);
+		return content;
+	end;
+end;
+
+function io.writeFile(path, content, mode)
+	local mode = mode or "w+b";
+	local file = io.open(path, mode);
+	if file then
+		if file:write(content) == nil then
+			return false;
+		end;
+		io.close(file);
+		return true;
+	else
+		return false;
+	end;
+end;
+
+function io.pathInfo(path)
+	local pos = string.len(path);
+	local extpos = pos + 1;
+	while pos > 0 do
+		local b = string.byte(path, pos);
+		if b == 46 then
+			extpos = pos;
+		elseif b == 47 then
+			break;
+		end;
+		pos = pos - 1;
+	end;
+	local dirname = string.sub(path, 1, pos);
+	local filename = string.sub(path, pos + 1);
+	extpos = extpos - pos;
+	local basename = string.sub(filename, 1, extpos - 1);
+	local extname = string.sub(filename, extpos);
+	return {
+		dirname = dirname,
+		filename = filename,
+		basename = basename,
+		extname = extname
+	};
+end;
+
+function io.fileSize(path)
+	local size = 0;
+	local file = io.open(path, "r");
+	if file then
+		local current = file:seek();
+		size = file:seek("end");
+		file:seek("set", current);
+		io.close(file);
+	end;
+	return size;
+end;
+
+function io.readStream(path, offset, len)
+	local file, str = io.open(path, "r");
+	if file then
+		local current = file:seek();
+		file:seek("set", offset);
+		str = file:read(len);
+		file:seek("set", current);
+		io.close(file);
+	end;
+	return str;
+end;
